21ca188fde07f0af3470e02802d0a4977a281eac
Babak-H <babak.habibnejad@gmail.com>
2024-02-21 09:21:00 +0100

    Update k8s-service-account.tf

--- AWS/19-EKS/kubernetes-cluster/k8s-service-account.tf
+++ AWS/19-EKS/kubernetes-cluster/k8s-service-account.tf
@@ -1,6 +1,7 @@
 # create a kubernetes service account and annotate it with the role that allows reading s3 via openID
 resource "kubernetes_service_account_v1" "irsa-demo-sa" {
   depends_on = [ aws_iam_role_policy_attachment.irsa-iam-role-policy-attach ]
+  namespace = "default" # var.serviceaccount_namespace
   metadata {
     name = "irsa-demo-sa"
     annotations = {
